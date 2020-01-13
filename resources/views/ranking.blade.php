1fc155ddd4df833fefd1dbafd9f2581ffa08c381
sofia290 <43052291+sofia290@users.noreply.github.com>
2020-01-13 10:40:15 -0300

    Create ranking.blade.php

---
+++ resources/views/ranking.blade.php
@@ -0,0 +1,56 @@
+  <div class="contenedorranking">
+      <div class="Principal">
+        <div class="PrimerPuesto">
+          <img class="foto" src="https://xavierferras.com/wp-content/uploads/2019/02/266-Persona.jpg" alt="">
+          <p>nombre: juana</p>
+          <p>punto:20.000</p>
+          <p>nivel:100</p>
+        </div>
+      </div>
+      <div class="secundario">
+        <div class="usuario">
+          <img class="foto" src="https://xavierferras.com/wp-content/uploads/2019/02/266-Persona.jpg" alt="">
+          <p>nombre: juana</p>
+          <p>punto:20.000</p>
+          <p>nivel:100</p>
+        </div>
+        <div class="usario">
+          <img class="foto" src="https://xavierferras.com/wp-content/uploads/2019/02/266-Persona.jpg" alt="">
+          <p>nombre: juana</p>
+          <p>punto:20.000</p>
+          <p>nivel:100</p>
+        </div>
+      </div>
+      </div>
+
+    <table class="highlight">
+        <thead>
+          <tr >
+
+              <th>Puesto</th>
+              <th>Usuario</th>
+              <th>puntos</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+
+            <td>4</td>
+            <td><a href="#">Eclair</a></td>
+            <td>8.000</td>
+          </tr>
+          <tr>
+
+            <td>5</td>
+            <td><a href="#">Jellybean</a></td>
+            <td>7.000</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><a href="#">Lollipop</a></td>
+            <td>6.000</td>
+          </tr>
+        </tbody>
+      </table>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
